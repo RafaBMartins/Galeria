bee96e7de3ff0b59f8f3d321df916591ae61927d
20201tiimi0250 <rafaelmartinscouto@gmail.com>
2023-03-31 11:08:41 -0300

    31/03/2023 parei no passo 8 do roteiro

--- app/src/main/java/martins/barbosa/rafael/galeria/activity/NewItemActivity.java
+++ app/src/main/java/martins/barbosa/rafael/galeria/activity/NewItemActivity.java
@@ -1,10 +1,16 @@
-package martins.barbosa.rafael.galeria;
+package martins.barbosa.rafael.galeria.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import martins.barbosa.rafael.galeria.R;
+
 public class NewItemActivity extends AppCompatActivity {
+
+    static int PHOTO_PICKER_REQUEST=1;
+    Uri photoSelected = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
