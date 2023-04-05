ee33027143cf7a6c49cfeb32f3db8a92ed0728e1
20201tiimi0250 <rafaelmartinscouto@gmail.com>
2023-04-05 12:49:52 -0300

    05/03/2023 terminado

--- app/src/main/java/martins/barbosa/rafael/galeria/activity/MainActivity.java
+++ app/src/main/java/martins/barbosa/rafael/galeria/activity/MainActivity.java
@@ -1,18 +1,31 @@
 package martins.barbosa.rafael.galeria.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import martins.barbosa.rafael.galeria.R;
+import martins.barbosa.rafael.galeria.adapter.MyAdapter;
+import martins.barbosa.rafael.galeria.model.MyItem;
 
 public class MainActivity extends AppCompatActivity {
 
     static int NEW_ITEM_REQUEST =1;
+    List<MyItem> itens = new ArrayList<>();
+    MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +40,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(i, NEW_ITEM_REQUEST);
             }
         });
+
+        RecyclerView rvItens = findViewById(R.id.rvItens);
+
+        myAdapter = new MyAdapter(MainActivity.this, itens);
+        rvItens.setAdapter(myAdapter);
+
+        rvItens.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        rvItens.setLayoutManager(layoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rvItens.getContext(), DividerItemDecoration.VERTICAL);
+        rvItens.addItemDecoration(dividerItemDecoration);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == NEW_ITEM_REQUEST) {
+            if(resultCode == Activity.RESULT_OK) {
+                MyItem myItem = new MyItem();
+                myItem.title = data.getStringExtra("title");
+                myItem.description = data.getStringExtra("description");
+                myItem.photo = data.getData();
+
+                itens.add(myItem);
+                myAdapter.notifyItemInserted(itens.size()-1);
+            }
+        }
     }
 }
---
+++ app/src/main/java/martins/barbosa/rafael/galeria/model/MyItem.java
@@ -0,0 +1,10 @@
+package martins.barbosa.rafael.galeria.model;
+
+import android.net.Uri;
+
+public class MyItem {
+
+    public Uri photo;
+    public String title;
+    public String description;
+}
